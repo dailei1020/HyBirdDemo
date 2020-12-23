32ba8a56f298ac4c97314d4d9a5060a5a9f289d0
dailei <dailei@coocaa.com>
2020-12-23 21:57:24 +0800

    提交一个webview的例子

--- app/src/main/java/com/example/hybirddemo/MainActivity.kt
+++ app/src/main/java/com/example/hybirddemo/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.hybirddemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mRootWebView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRootWebView = rootWebView
+        mRootWebView.loadUrl("https://www.baidu.com")
+        mRootWebView.webViewClient = WebViewClient()
+
+        val webSettings:WebSettings = mRootWebView.settings
+        //设置支持缩放
+        webSettings.setSupportZoom(true)
+        webSettings.builtInZoomControls = true
+        webSettings.displayZoomControls = true
+
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mRootWebView.canGoBack()) {
+                mRootWebView.goBack()
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
