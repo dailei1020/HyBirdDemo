c848d1db8279613e44fbc52b4c7311d4c32528c6
dailei <dailei@coocaa.com>
2020-12-23 22:13:38 +0800

    提交webChromeClient常用方法

--- app/src/main/java/com/example/hybirddemo/MainActivity.kt
+++ app/src/main/java/com/example/hybirddemo/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.hybirddemo
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.KeyEvent
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,14 +22,38 @@ class MainActivity : AppCompatActivity() {
 
         mRootWebView = rootWebView
         mRootWebView.loadUrl("https://www.baidu.com")
-        mRootWebView.webViewClient = WebViewClient()
+        mRootWebView.webViewClient = object : WebViewClient(){
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+            }
 
-        val webSettings:WebSettings = mRootWebView.settings
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+            }
+        }
+
+
+        mRootWebView.webChromeClient = object : WebChromeClient() {
+            //获取网页进度
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+            }
+            //获取网页title
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                super.onReceivedTitle(view, title)
+            }
+        }
+
+        setZoom()
+
+    }
+
+    private fun setZoom() {
+        val webSettings: WebSettings = mRootWebView.settings
         //设置支持缩放
         webSettings.setSupportZoom(true)
         webSettings.builtInZoomControls = true
         webSettings.displayZoomControls = true
-
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
